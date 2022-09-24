f85f0463f2dad04936ec5ea3decf233740c96704
Raitokawa <agfa1508@gmail.com>
2022-09-24 19:10:44 +0700

    Add all my files

---
+++ Assets/Script/EnemyStat.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStat : BaseStatSystem
+{
+    public override void Die()
+    {
+        base.Die();
+
+
+        Destroy(gameObject);
+    }
+}
